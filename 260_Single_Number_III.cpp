75ecaf17fdc0f1a4a9b5265d536344453710e890
kanu-CH <121243650+kanu-CH@users.noreply.github.com>
2024-05-31 22:46:21 +0530

    Create 260_Single_Number_III.cpp

---
+++ 260_Single_Number_III.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        long ans = 0;
+        for(auto &num: nums)
+            ans ^= num;
+
+        int check = ans & (-ans);
+        int grpA = 0;
+        int grpB = 0;
+
+        for(auto &num: nums){
+            if(check & num)
+                grpA ^= num;
+            else
+                grpB ^= num;  
+        }
+        return {grpA,grpB};
+    }
+};

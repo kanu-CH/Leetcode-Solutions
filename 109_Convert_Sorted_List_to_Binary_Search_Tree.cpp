1248f08ed029a5df2dc6e7918ed774beb8716148
kanu-CH <121243650+kanu-CH@users.noreply.github.com>
2024-07-01 18:57:11 +0530

    Create 109_Convert_Sorted_List_to_Binary_Search_Tree.cpp

---
+++ 109_Convert_Sorted_List_to_Binary_Search_Tree.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    TreeNode* bst(ListNode* head, ListNode* tail){
+        if(head==tail){
+            return NULL;
+        }
+        ListNode* slow=head;
+        ListNode* fast=head;
+
+        while(fast != tail && fast->next != tail){
+            fast=fast->next->next;
+            slow=slow->next;
+        }
+
+        TreeNode* node=new TreeNode(slow->val);
+        node->left=bst(head,slow);
+        node->right=bst(slow->next,tail);
+        return node;
+    }
+
+public:
+    TreeNode* sortedListToBST(ListNode* head) {
+        if(head==NULL){
+            return NULL;
+        }
+        return bst(head,NULL);
+    }
+};

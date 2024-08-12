6cc9926827d18c0142502b8d75a87086ea271670
Kanika Chaudhary <121243650+kanu-CH@users.noreply.github.com>
2024-08-12 19:53:47 +0530

    Update 509_Fibonacci_Number.cpp

--- 509_Fibonacci_Number.cpp
+++ 509_Fibonacci_Number.cpp
@@ -1,11 +1,44 @@
+//recursion
+class Solution {
+public:
+    int fib(int n) {
+        if (n == 0) return 0;
+	else if (n == 1) return 1;
+	else return fib(n - 1) + fib(n - 2);
+    }
+};
+
+//dp bottom up approach
 class Solution {
 public:
     int fib(int n) {
         if (n == 0)
-			return 0;
-		else if (n == 1)
-			return 1;
-		else
-			return fib(n - 1) + fib(n - 2);
+            return 0;
+        if (n == 1)
+            return 1;
+        vector<int> dp(n + 1);
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++)
+            dp[i] = dp[i - 1] + dp[i - 2];
+        return dp[n];
+    }
+};
+
+//dp top down approach
+class Solution {
+public:
+    int fib(int n) {
+        // base case
+        if (n <= 1)
+            return n;
+        vector<int> dp(n + 1, -1);
+
+        if (dp[n] != -1)
+            return dp[n];
+
+        dp[n] = fib(n - 1) + fib(n - 2);
+        return dp[n];
     }
 };

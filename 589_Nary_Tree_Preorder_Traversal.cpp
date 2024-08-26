24e785a3b5ec8e08f41857a3ac9aee586b28f2d5
Kanika Chaudhary <121243650+kanu-CH@users.noreply.github.com>
2024-08-26 18:14:22 +0530

    Create 589_Nary_Tree_Preorder_Traversal.cpp

---
+++ 589_Nary_Tree_Preorder_Traversal.cpp
@@ -0,0 +1,37 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    vector<int> res;
+    void preOrder(Node* root){
+        if(!root)
+            return;
+        res.push_back(root->val);
+        
+        for(auto child : root->children)
+            preOrder(child);
+        
+    }
+public:
+    vector<int> preorder(Node* root) {
+        preOrder(root);
+        return res;
+    }
+};

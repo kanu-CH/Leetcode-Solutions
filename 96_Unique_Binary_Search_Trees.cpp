21e523aee1bd19de4f97390a5da0f32ab8b06b76
Kanika Chaudhary <121243650+kanu-CH@users.noreply.github.com>
2024-09-23 20:04:22 +0530

    Create 96_Unique_Binary_Search_Trees.cpp

---
+++ 96_Unique_Binary_Search_Trees.cpp
@@ -0,0 +1,36 @@
+// top down approach
+class Solution {
+public:
+    int solve(int n, vector<int> &dp){
+        if(n<=1) return 1;
+
+        if(dp[n] != -1) return dp[n];
+        
+        int ans = 0;
+
+        for(int i=1; i<=n; i++){
+            ans += solve(i-1, dp) * solve(n-i, dp);
+        }
+        return dp[n] = ans;
+    }
+    int numTrees(int n) {
+        vector<int> dp(n+1,-1);
+        return solve(n,dp);
+    }
+};
+
+// bottom up approach
+class Solution {
+public:
+    int numTrees(int n) {
+        vector<int> dp(n+1,0);
+        dp[0] = dp[1] = 1;
+
+        for(int i=2; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                dp[i] += dp[j-1] * dp[i-j];
+            }
+        }
+        return dp[n];
+    }
+};
